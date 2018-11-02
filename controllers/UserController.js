384afaaa0af3da84b3a97f32356b35eb7f9dc086
Almir Junior <almirjr944@gmail.com>
2018-11-02 12:22:31 -0300

    FileReader e Promise

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,10 +11,54 @@ class UserController{
     onSubmit(){
         this.formEl.addEventListener("submit",e =>{
             e.preventDefault();
-            console.log(this.getValues());
-            this.addLine(this.getValues());
+
+            let values = this.getValues();
+
+            this.getPhoto().then(
+                (content)=>{
+                    values.photo = content;
+                    this.addLine(values);
+                },
+                (e)=>{
+                    console.error(e);
+                }    
+            );
+
+            this.getPhoto((content)=>{
+                values.photo = content;
+                console.log(values);
+                this.addLine(values);
+            });
+
         });
          
+    }
+
+    getPhoto(){
+        
+        return new Promise((resolve,reject)=>{
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item=>{
+                if(item.name === 'photo'){
+                    return item;
+                }
+            })
+    
+            let file = elements[0].files[0];
+            
+            fileReader.onload = ()=>{
+                
+                resolve(fileReader.result);
+            };
+
+            fileReader.onerror = ()=>{
+                reject(e);
+            }
+    
+            fileReader.readAsDataURL(file);
+
+        });
     }
 
     getValues(){
@@ -39,7 +83,7 @@ class UserController{
     addLine(dataUser){
     
         this.tableEl.innerHTML += `<tr>
-                            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+                            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
                             <td>${dataUser.name}</td>
                             <td>${dataUser.email}</td>
                             <td>${dataUser.admin}</td>
